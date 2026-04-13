b7497bdd43fae1db22f76de0db4972de8b7b8cf7
kym-tech-dot <paul100kimani@gmail.com>
2026-04-14 00:15:48 +0300

    Configure Puppeteer cache directory
    Add Puppeteer configuration to specify cache directory.

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -1,2 +1,11 @@
+const { join } = require('path');
+
+/**
+ * @type {import("puppeteer").Configuration}
+ */
+module.exports = {
+  // This tells Puppeteer to save Chrome inside your project folder so Render can find it
+  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
+};
 
 
